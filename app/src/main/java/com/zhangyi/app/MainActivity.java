164f93018b46b735a370fdde7fcfc6b3c5a79cbe
zhangyi <zhangyi22@kingsoft.com>
2022-01-18 10:31:45 +0800

    添加demo

---
+++ app/src/main/java/com/zhangyi/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zhangyi.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zhangyi.dorpdown.DropDownView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DropDownView dropDownView;
+    }
+}
